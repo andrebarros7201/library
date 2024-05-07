0a5ec2ea6b962c9e9a47be85f6a5bf06f51b8167
Andre Barros <andrebarros7201@gmail.com>
2024-05-07 21:21:51 +0100

    remove book from display

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function addBookToLibrary(book) {
 
 const displayBooks = () => {
     const books = document.querySelector('.books');
-  myLibrary.forEach((book) => {
+    myLibrary.forEach((book) => {
     const bookCard = document.createElement("div");
     bookCard.classList.add("book");
 
@@ -37,22 +37,38 @@ const displayBooks = () => {
     const read = document.createElement("p");
     read.textContent = `Is read: ${book.read}`;
 
+    const buttonDelete = document.createElement('button');
+    buttonDelete.classList.add('delete');
+    buttonDelete.textContent = 'Delete';
+    
+    buttonDelete.addEventListener('click', () =>{
+        const index = myLibrary.indexOf(book)
+        console.log(index);
+        myLibrary.splice(index, 1);
+        resetDisplay();
+        displayBooks();
+    });
+
     bookCard.appendChild(title);
     bookCard.appendChild(author);
     bookCard.appendChild(pages);
     bookCard.appendChild(read);
+    bookCard.appendChild(buttonDelete);
 
     books.appendChild(bookCard);
   });
 };
 
+const narnia = new Book("The Chronicles of Narnia: The Lion, the Witch, and the Wardrobe", "C.S. Lewis", 192, false);
+const percyJackson = new Book("Percy Jackson & the Olympians: The Lightning Thief", "Rick Riordan", 384, true);
+const harryPotter = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 223, false);
+const artemisFowl = new Book("Artemis Fowl", "Eoin Colfer", 288, true);
 const hobbit = new Book("The Hobbit", "JRR Tolkien", 295, true);
 addBookToLibrary(hobbit);
-addBookToLibrary(hobbit);
-addBookToLibrary(hobbit);
-addBookToLibrary(hobbit);
-addBookToLibrary(hobbit);
-addBookToLibrary(hobbit);
+addBookToLibrary(artemisFowl)
+addBookToLibrary(narnia);
+addBookToLibrary(percyJackson);
+addBookToLibrary(harryPotter);
 
 console.log(myLibrary);
 
@@ -66,6 +82,12 @@ buttonOpenDialog.addEventListener('click', () =>{
 
 buttonCloseDialog.addEventListener('click', () =>{
     dialog.close();
-})
+});
+const resetDisplay = () =>{
+    const books = document.querySelector('.books')
+    while(books.firstChild){
+        books.removeChild(books.firstChild);
+    }
+}
 
 displayBooks();
